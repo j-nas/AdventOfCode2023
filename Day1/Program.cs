460bb32e88dffb693523fd83fd0f78e98b36936d
Jon Naske <jon@shorecel.com>
2023-12-04 17:57:01 -0800

    Day 1 Solved

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+
+var runningTotal = 0;
+
+input.ToList().ForEach(line =>
+{
+
+    var numbers = line
+        .ToCharArray()
+        .Select(c => int.TryParse(c.ToString(), out var number) ? number : -1)
+        .Where(n => n != -1);
+    
+    var first = numbers.First();
+    var last = numbers.Last();
+
+    var combinedDigits = int.Parse(first.ToString() + last.ToString());
+    
+    runningTotal += combinedDigits;
+
+});
+
+Console.WriteLine($"Total: {runningTotal}");
+Console.ReadKey();
+
+
+
+
+    
+    
+
